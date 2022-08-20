ccf25726d52cd6eea66fb461dbf3f28af441a71f
syedkaliff <38420044+syedkaliff@users.noreply.github.com>
2022-08-20 22:18:05 +0530

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -81,7 +81,7 @@ EOF
     
    provisioner "local-exec" {
     #command = "printf '%s\n' 2a '${self.public_ip} ansible_user=administrator' . x | ex /tmp/ansibleroles/hostfile"
-     "printf '\n${self.public_ip}' >> /tmp/ansibleroles/hostfile"
+    command = "printf '\n${self.public_ip}      ansible_user=administrator' >> /tmp/ansibleroles/hostfile"
      
   }
    
